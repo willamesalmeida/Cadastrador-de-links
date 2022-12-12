a2adc722b9405a3e91bfb04a75fa13af10bcac9a
*willamesalmeida <*willames5480@gmail.com>
2022-12-12 18:27:57 -0300

    Storing account into database.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const { Account } = require("../models");
 const router = express.Router();
 
-router.get('/sign-in', (req, res) => {
-    return res.json('Sign in')
-})
+const saltRounds = 10;
 
-router.get('/sign-up', (req, res) => {
-    return res.json('Sign up');
+router.get("/sign-in", (req, res) => {
+  return res.json("Sign in");
 });
 
+router.get("/sign-up", async (req, res) => {
+  const email = "willames@gmail.com";
+  const password = "12345";
+
+  const hash = bcrypt.hashSync(password, saltRounds);
+  const result = await Account.create({ email, password: hash});
+  console.log(result);
+  return res.json(result);
+});
 
 module.exports = router;
