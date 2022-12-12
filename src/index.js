056c4253760f3c307889de6db938d31175019def
*willamesalmeida <*willames5480@gmail.com>
2022-12-12 11:55:44 -0300

    Nodemon configured and auth controller created

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const express = require('express')
 
+const authController = require('./controllers/auth.js')
 const app = express()
 const port = 3001;
 
+app.use('/auth', authController);
+
 app.get('/', (req, res) => {
      return res.json('API runing...')
-} )
-
-
-
-
+}) ;
 
 
 app.listen(port, () => {
