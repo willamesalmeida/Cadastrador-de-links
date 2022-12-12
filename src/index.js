889b4f3d3e5a29e78a096b4980748276bb157b5c
*willamesalmeida <*willames5480@gmail.com>
2022-12-12 16:02:48 -0300

    Sequelize configured.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-const express = require('express')
+const express = require("express");
+const db = require("./models");
 
-const authController = require('./controllers/auth.js')
-const app = express()
+const authController = require("./controllers/auth.js");
+const app = express();
 const port = 3001;
 
-app.use('/auth', authController);
+app.use("/auth", authController);
 
-app.get('/', (req, res) => {
-     return res.json('API runing...')
-}) ;
+app.get("/", (req, res) => {
+  return res.json("API runing...");
+});
 
-
-app.listen(port, () => {
+db.sequelize.sync().then(() => {
+  app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-})
+  });
+});
